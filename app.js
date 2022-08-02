ee185266bc4f70d29c733e0e66bf1a4970106761
HoonYoo <102582678+HoonYoo@users.noreply.github.com>
2022-08-02 21:02:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.get("/", function(req, res) {
 });
 
 client.setConfig({
-  apiKey: "cb26de51717ca010a52fca4248d4b775-us14",
-  server: "us14",
+  apiKey: "",
+  server: "",
 });
 
 app.post("/", function(req, res) {
@@ -60,11 +60,5 @@ app.listen(process.env.PORT || 5000, function() {
 });
 
 
-
-// API key
-// cb26de51717ca010a52fca4248d4b775-us14
-
-// Listid
-// 34fb024009
 
 // {"name":"Freddie'\''s Favorite Hats","contact":{"company":"MailChimp","address1":"675 Ponce De Leon Ave NE","address2":"Suite 5000","city":"Atlanta","state":"GA","zip":"30308","country":"US","phone":""},"permission_reminder":"You'\''re receiving this email because you signed up for updates about Freddie'\''s newest hats.","campaign_defaults":{"from_name":"Freddie","from_email":"freddie@freddiehats.com","subject":"","language":"en"},"email_type_option":true}
